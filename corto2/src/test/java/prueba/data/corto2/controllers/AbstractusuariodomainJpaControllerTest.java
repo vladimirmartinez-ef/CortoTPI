cc1fc2961b4e71d964eb9075764d26e55fc6a45f
vladimirmartinez-ef <vemf.vemf@hotmail.com>
2021-04-01 17:22:18 -0600

    haciendo pruebas--delete jacoco

--- corto2/src/test/java/prueba/data/corto2/controllers/AbstractusuariodomainJpaControllerTest.java
+++ corto2/src/test/java/prueba/data/corto2/controllers/AbstractusuariodomainJpaControllerTest.java
@@ -7,6 +7,13 @@ package prueba.data.corto2.controllers;
 
 import java.util.List;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,9 +21,11 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import prueba.data.corto2.controllers.exceptions.NonexistentEntityException;
+import prueba.data.corto2.controllers.exceptions.PreexistingEntityException;
 import prueba.data.corto2.entitys.Abstractusuariodomain;
-
 
 /**
  *
@@ -24,136 +33,153 @@ import prueba.data.corto2.entitys.Abstractusuariodomain;
  */
 @ExtendWith(MockitoExtension.class)
 public class AbstractusuariodomainJpaControllerTest {
-    
+
+    EntityManager mockEm = Mockito.mock(EntityManager.class);
+    EntityTransaction mockTX = Mockito.mock(EntityTransaction.class);
+    final static EntityManagerFactory mockEmf = Mockito.mock(EntityManagerFactory.class);
+
+    AbstractusuariodomainJpaController cut = new AbstractusuariodomainJpaController(mockEmf);
+
     public AbstractusuariodomainJpaControllerTest() {
     }
-    
+
     @BeforeAll
     public static void setUpClass() {
     }
-    
+
     @AfterAll
     public static void tearDownClass() {
     }
-    
+
     @BeforeEach
     public void setUp() {
     }
-    
+
     @AfterEach
     public void tearDown() {
     }
 
     /**
-     * Test of getEntityManager method, of class AbstractusuariodomainJpaController.
+     * Test of getEntityManager method, of class
+     * AbstractusuariodomainJpaController.
      */
     @Test
     public void testGetEntityManager() {
         System.out.println("getEntityManager");
-        AbstractusuariodomainJpaController instance = null;
-        EntityManager expResult = null;
-        EntityManager result = instance.getEntityManager();
-        assertEquals(expResult, result);
+        Mockito.when(mockEmf.createEntityManager()).thenReturn(mockEm);
+        cut.getEntityManager();
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        // fail("Dara error por que asi dice aqui");
     }
 
-    /**
-     * Test of create method, of class AbstractusuariodomainJpaController.
-     */
     @Test
     public void testCreate() throws Exception {
         System.out.println("create");
-        Abstractusuariodomain abstractusuariodomain = null;
-        AbstractusuariodomainJpaController instance = null;
-        instance.create(abstractusuariodomain);
+        Mockito.when(cut.getEntityManager()).thenReturn(mockEm);
+        Mockito.when(mockEm.getTransaction()).thenReturn(mockTX);
+        Abstractusuariodomain r = new Abstractusuariodomain();
+        r.setLogin("1");
+        r.setNombre("nombre");
+        r.setApellido1("apellido1");
+        r.setApellido2("apellido2");
+        r.setEmail("email@hello.com");
+        r.setPassword("hhs3dvdf3");
+        r.setEstadoUsuario(Short.parseShort("1"));
+        r.setCiudad("ciudad");
+        r.setDireccion("direccon");
+        r.setCodigoPostal(3);
+        //Abstractusuariodomain mockR = Mockito.mock(Abstractusuariodomain.class);
+        cut.create(r);
+        Mockito.verify(mockEm, Mockito.times(1)).persist(Mockito.any());
+        //error exception
+        try {
+            Mockito.doThrow(PreexistingEntityException.class).when(mockEm).persist(Mockito.any());
+            Mockito.when(cut.findAbstractusuariodomain(Mockito.any())).thenReturn(r);
+            cut.create(r);
+        } catch (Exception e) {
+
+        }
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        //fail("Dara error por que asi dice aqui");
     }
 
-    /**
-     * Test of edit method, of class AbstractusuariodomainJpaController.
-     */
     @Test
     public void testEdit() throws Exception {
         System.out.println("edit");
-        Abstractusuariodomain abstractusuariodomain = null;
-        AbstractusuariodomainJpaController instance = null;
-        instance.edit(abstractusuariodomain);
+        Mockito.when(cut.getEntityManager()).thenReturn(mockEm);
+        Mockito.when(mockEm.getTransaction()).thenReturn(mockTX);
+        Abstractusuariodomain mockR = Mockito.mock(Abstractusuariodomain.class);
+        cut.edit(mockR);
+        Mockito.verify(mockEm, Mockito.times(1)).merge(Mockito.any());
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        //fail("Dara error por que asi dice aqui");
     }
 
-    /**
-     * Test of destroy method, of class AbstractusuariodomainJpaController.
-     */
     @Test
     public void testDestroy() throws Exception {
         System.out.println("destroy");
-        String id = "";
-        AbstractusuariodomainJpaController instance = null;
-        instance.destroy(id);
+        Mockito.when(cut.getEntityManager()).thenReturn(mockEm);
+        Mockito.when(mockEm.getTransaction()).thenReturn(mockTX);
+        Abstractusuariodomain mockR = Mockito.mock(Abstractusuariodomain.class);
+        Mockito.when(mockEm.getReference(Abstractusuariodomain.class, "user")).thenReturn(mockR);
+        Mockito.when(mockR.getLogin()).thenReturn("user");
+        cut.destroy("user");
+        Mockito.verify(mockEm, Mockito.times(1)).remove(Mockito.any());
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        //fail("Dara error por que asi dice aqui");
     }
 
-    /**
-     * Test of findAbstractusuariodomainEntities method, of class AbstractusuariodomainJpaController.
-     */
     @Test
-    public void testFindAbstractusuariodomainEntities_0args() {
+    public void testFindAbstractusuariodomainEntities() throws Exception {
         System.out.println("findAbstractusuariodomainEntities");
-        AbstractusuariodomainJpaController instance = null;
-        List<Abstractusuariodomain> expResult = null;
-        List<Abstractusuariodomain> result = instance.findAbstractusuariodomainEntities();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        List<Abstractusuariodomain> l = Mockito.mock(List.class);
+        Mockito.when(cut.getEntityManager()).thenReturn(mockEm);
+        CriteriaQuery mockCRQ = Mockito.mock(CriteriaQuery.class);
+        CriteriaBuilder mockCRBDR = Mockito.mock(CriteriaBuilder.class);
+        Mockito.when(mockEm.getCriteriaBuilder()).thenReturn(mockCRBDR);
+        Mockito.when(mockCRBDR.createQuery()).thenReturn(mockCRQ);
+        Root mockROOT = Mockito.mock(Root.class);
+        Mockito.when(mockCRQ.from(Abstractusuariodomain.class)).thenReturn(mockROOT);
+        TypedQuery mockQR = Mockito.mock(TypedQuery.class);
+        Mockito.when(mockEm.createQuery(mockCRQ)).thenReturn(mockQR);
+        Query mockQRY = Mockito.mock(Query.class);
+        cut.findAbstractusuariodomainEntities();
     }
 
-    /**
-     * Test of findAbstractusuariodomainEntities method, of class AbstractusuariodomainJpaController.
-     */
     @Test
-    public void testFindAbstractusuariodomainEntities_int_int() {
-        System.out.println("findAbstractusuariodomainEntities");
-        int maxResults = 0;
-        int firstResult = 0;
-        AbstractusuariodomainJpaController instance = null;
-        List<Abstractusuariodomain> expResult = null;
-        List<Abstractusuariodomain> result = instance.findAbstractusuariodomainEntities(maxResults, firstResult);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testFindAbstractusuariodomainEntities_int_int() throws Exception {
+        System.out.println("findAbstractusuariodomainEntities_int_int");
+        List<Abstractusuariodomain> l = Mockito.mock(List.class);
+        Mockito.when(cut.getEntityManager()).thenReturn(mockEm);
+        CriteriaQuery mockCRQ = Mockito.mock(CriteriaQuery.class);
+        CriteriaBuilder mockCRBDR = Mockito.mock(CriteriaBuilder.class);
+        Mockito.when(mockEm.getCriteriaBuilder()).thenReturn(mockCRBDR);
+        Mockito.when(mockCRBDR.createQuery()).thenReturn(mockCRQ);
+        Root mockROOT = Mockito.mock(Root.class);
+        Mockito.when(mockCRQ.from(Abstractusuariodomain.class)).thenReturn(mockROOT);
+        TypedQuery mockQR = Mockito.mock(TypedQuery.class);
+        Mockito.when(mockEm.createQuery(mockCRQ)).thenReturn(mockQR);
+        cut.findAbstractusuariodomainEntities(100, 1);
     }
 
-    /**
-     * Test of findAbstractusuariodomain method, of class AbstractusuariodomainJpaController.
-     */
     @Test
     public void testFindAbstractusuariodomain() {
         System.out.println("findAbstractusuariodomain");
-        String id = "";
-        AbstractusuariodomainJpaController instance = null;
-        Abstractusuariodomain expResult = null;
-        Abstractusuariodomain result = instance.findAbstractusuariodomain(id);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Mockito.when(cut.getEntityManager()).thenReturn(mockEm);
+        Abstractusuariodomain mockR = Mockito.mock(Abstractusuariodomain.class);
+        Mockito.when(mockEm.find(Abstractusuariodomain.class, "hola")).thenReturn(mockR);
+        cut.findAbstractusuariodomain("hola");
     }
 
     /**
-     * Test of getAbstractusuariodomainCount method, of class AbstractusuariodomainJpaController.
+     * Test of getAbstractusuariodomainCount method, of class
+     * AbstractusuariodomainJpaController.
      */
     @Test
     public void testGetAbstractusuariodomainCount() {
         System.out.println("getAbstractusuariodomainCount");
-        AbstractusuariodomainJpaController instance = null;
-        int expResult = 0;
-        int result = instance.getAbstractusuariodomainCount();
-        assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        //fail("The test case is a prototype.");
     }
-    
+
 }
